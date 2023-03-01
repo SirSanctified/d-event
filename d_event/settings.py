1e8a2dd29d9c6a3547e74e4886e737ba9c0caf9e
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-01 11:56:45 +0200

    Prepare for deployment

--- d_event/settings.py
+++ d_event/settings.py
@@ -146,7 +146,7 @@ STATICFILES_DIRS = [
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/'
+MEDIA_URL = '/mediafiles/'
 MEDIA_ROOT = BASE_DIR / 'mediafiles'
 
 
