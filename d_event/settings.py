decec493cfbcc4abc7a6eba271a0f6881a6d31fa
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-01 11:53:22 +0200

    Prepare for deployment

--- d_event/settings.py
+++ d_event/settings.py
@@ -141,7 +141,7 @@ USE_TZ = True
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'media')
+    os.path.join(BASE_DIR, 'mediafiles')
 ]
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
