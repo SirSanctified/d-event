434dc9e316f7c5d1545a4af189d757d1ba2fca01
Pritchard Mambambo <mambambopritchard@gmail.com>
2023-03-03 09:09:03 +0200

    Update s3 settings

--- d_event/settings.py
+++ d_event/settings.py
@@ -148,7 +148,7 @@ else:
     AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
-    AWS_DEFAULT_ACL = 'public-read'
+    AWS_DEFAULT_ACL = None
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
     AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
 
--- main/storage_backends.py
+++ main/storage_backends.py
@@ -1,4 +1,3 @@
-from abc import ABC
 
 from storages.backends.s3boto3 import S3Boto3Storage
 
